14f39b93c76954f0904e936030ce60e3a2b44c54
김예슬 <kim-yeseul@gim-yeseul-ui-MacBookPro-2.local>
2020-10-24 22:55:20 +0900

    search를 위한 form 추가

--- rooms/views.py
+++ rooms/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator, EmptyPage
+from django_countries import countries
 from . import models
 from django.views.generic import ListView, DetailView
 from django.urls import reverse
@@ -87,5 +88,46 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    print(request)
-    return render(request, "rooms/search.html")
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": super_host,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    return render(request,
+                  "rooms/search.html",
+                  { **form, **choices})     # form
